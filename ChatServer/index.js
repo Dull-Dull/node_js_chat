538c2c0fc0b01136e02ab97046905b76d9537a4c
김준엽 [melony] <melony@nexon-gt.com>
2019-05-24 08:44:04 +0900

    ?? ?? ????

--- ChatServer/index.js
+++ ChatServer/index.js
@@ -2,14 +2,27 @@ var app = require('express')();
 var http = require('http').createServer( app );
 var io = require('socket.io')(http);
 
-app.get('/', ( req, res) => {
-    res.send('<h1>Hello World</h1>');
+// app.get('/', ( req, res) => {
+//     res.send('<h1>Hello World</h1>');
+// });
+
+app.post('/Login', (req, res)=>{
+    Console.log('login user', req.ip );
 });
 
 io.on('connection', (socket)=>{
+    socket.broadcast.emit
+
     console.log('a user connected');
+
+    socket.on( 'disconnect', ()=>{
+        console.log('user disconnected');
+
+        
+    });
+
 });
 
-http.listen( 80, ()=>{
+http.listen( 19900, ()=>{
     console.log('server is run!!!');
 });
